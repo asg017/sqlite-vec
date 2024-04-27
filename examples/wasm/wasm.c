6c97750e48bbb91277e96a2fc050ebba8546cb75
Alex Garcia <alexsebastian.garcia@gmail.com>
2024-04-27 12:05:50 -0700

    wasm tests and fixes

--- examples/wasm/wasm.c
+++ examples/wasm/wasm.c
@@ -1,6 +1,6 @@
 #include "sqlite3.h"
 #include "sqlite-vec.h"
 
-int sqlite3_wasm_extra_init(const char *) {
-  sqlite3_auto_extension((void (*)(void)) sqlite3_vec_init);
+int sqlite3_wasm_extra_init(const char * unused) {
+  return sqlite3_auto_extension((void (*)(void)) sqlite3_vec_init);
 }
---
+++ tests/test-wasm.mjs
@@ -0,0 +1,10 @@
+async function main() {
+  const { default: init } = await import("../dist/.wasm/sqlite3.mjs");
+  const sqlite3 = await init();
+  const vec_version = new sqlite3.oo1.DB(":memory:").selectValue(
+    "select vec_version()"
+  );
+  console.log(vec_version);
+}
+
+main();
