217907162423bc45f382bd3867e22db50e89bf2e
Sheldon Robinson <sheldon.robinson@gmail.com>
2024-11-01 11:51:16 -0400

    Fix redifinition of u64

--- sqlite-vec.c
+++ sqlite-vec.c
@@ -65,13 +65,7 @@ typedef u_int64_t uint64_t;
 #endif
 #endif
 
-typedef int8_t i8;
-typedef uint8_t u8;
-typedef int16_t i16;
 typedef int32_t i32;
-typedef sqlite3_int64 i64;
-typedef uint32_t u32;
-typedef uint64_t u64;
 typedef float f32;
 typedef size_t usize;
 
