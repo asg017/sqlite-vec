6a4b640c18ae30408d71a1353053b455862e8832
Joel Koch <joel@joelkoch.dev>
2025-09-30 23:31:11 +0200

    Remove octo_fetch

--- bindings/elixir/mix.exs
+++ bindings/elixir/mix.exs
@@ -11,10 +11,6 @@ defmodule SqliteVec.MixProject do
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: Mix.compilers() ++ [:download_sqlite_vec],
-      aliases: [
-        "compile.download_sqlite_vec": &download_sqlite_vec/1
-      ],
       preferred_cli_env: [
         "test.watch": :test
       ],
@@ -27,31 +23,8 @@ defmodule SqliteVec.MixProject do
     ]
   end
 
-  defp download_sqlite_vec(_) do
-    version = Application.get_env(:sqlite_vec, :version, SqliteVec.Downloader.default_version())
-
-    output_dir = Path.join(__DIR__, "priv/#{version}")
-    File.mkdir_p!(output_dir)
-
-    case SqliteVec.download(output_dir, version) do
-      :skip ->
-        :ok
-
-      {:ok, _successful_files, []} ->
-        :ok
-
-      {:ok, _successful_files, failed_files} ->
-        message = "failed to download: " <> Enum.join(failed_files, ", ")
-        raise(message)
-
-      {:error, message} ->
-        raise(message)
-    end
-  end
-
   defp deps do
     [
-      {:octo_fetch, "~> 0.4.0"},
       {:ecto, "~> 3.0", optional: true},
       {:nx, "~> 0.9", optional: true},
       {:ecto_sql, "~> 3.0", only: :test},
--- bindings/elixir/test/sqlite_vec_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SqliteVecTest do
-  use ExUnit.Case
-
-  @tag :slow
-  test "supported downloads should work" do
-    assert :ok = OctoFetch.Test.test_all_supported_downloads(SqliteVec.Downloader)
-  end
-end
